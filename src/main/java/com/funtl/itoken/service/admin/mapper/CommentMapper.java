baef0b548de1061cd7caaeace8db6e23f7dd6d58
xuxingping <xuxingping@cyou-inc.com>
2019-11-13 22:48:49 +0800

    服务提供者

---
+++ src/main/java/com/funtl/itoken/service/admin/mapper/CommentMapper.java
@@ -0,0 +1,7 @@
+package com.funtl.itoken.service.admin.mapper;
+
+import com.funtl.itoken.service.admin.domain.Comment;
+import tk.mybatis.mapper.Mapper;
+
+public interface CommentMapper extends Mapper<Comment> {
+}
